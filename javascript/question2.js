85e26eb6b91ba348cd392ae3f8cf96ce1f24aea5
Kanika Sharma <kanikasharma9006@gmail.com>
2025-11-14 23:17:34 +0530

    arthmatic operator

---
+++ javascript/question2.js
@@ -0,0 +1,4 @@
+const a=5
+const b=5
+const c=1
+console.log(a+b+c);
---
+++ javascript/question3.js
@@ -0,0 +1,8 @@
+let a=2
+let b=6
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
