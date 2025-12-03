a88dbf0129d8dd231ed8de61910f9154b16b3add
Kanika Sharma <kanikasharma9006@gmail.com>
2025-12-04 00:44:09 +0530

    check sum is odd or even

---
+++ javascript/question6.js
@@ -0,0 +1,12 @@
+let a=61
+if(a<18)
+{
+    console.log("you are minor")
+}
+else if(a>=18&&a<=60)
+{
+    console.log("you are adult")
+}
+else{
+    console.log("you are senior citizen")
+}
---
+++ javascript/question7.js
@@ -0,0 +1,10 @@
+let a=10
+let b=10
+let sum=a+b
+if(sum%2==0)
+{
+    console.log("even")
+}
+else{
+    console.log("odd")
+}
