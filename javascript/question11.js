2a59c988878874ca40e739c374bc4fb65f0d2c1a
Kanika Sharma <kanikasharma9006@gmail.com>
2025-12-04 23:30:12 +0530

    basic calculator

---
+++ javascript/question11.js
@@ -0,0 +1,19 @@
+let a=20
+let b=40
+let c='*'
+if(c=='+')
+console.log(a+b)
+else if(c=='-')
+console.log(a-b)
+else if(c=='*')
+console.log(a*b)
+else if(c=='/')
+console.log(a/b)
+else{
+    console.log("invalid operation")
+}
+
+
+
+
+
