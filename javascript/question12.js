344cafda532aedc556a8291b0d16a646ecb50a68
Kanika Sharma <kanikasharma9006@gmail.com>
2025-12-08 21:23:42 +0530

    greeting system

---
+++ javascript/question12.js
@@ -0,0 +1,17 @@
+let name='kanika'
+let time=24
+if(time>=5&&time<=12)
+{
+    console.log('good morning '+ name)
+}
+else if(time>12&&time<=17)
+{
+    console.log('good afternoon '+name)
+}
+else if(time>17&&time<=21)
+{
+    console.log('good evening '+name)
+}
+else{
+    console.log('good night '+name)
+}
