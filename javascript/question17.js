1723b43cfb927c1df110e674816d6673c090e257
Kanika Sharma <kanikasharma9006@gmail.com>
2025-12-19 01:10:57 +0530

    fizzbuzz code in javascript

---
+++ javascript/question17.js
@@ -0,0 +1,16 @@
+
+// FizzBuzz (Multiple of Both) – Ask the user for a number.
+//  If it's a multiple of both 3 and 5, print “FizzBuzz”; if only 3, print “Fizz”; if only 5, print “Buzz”; 
+//  otherwise, print the number itself.
+ let a=20
+ if(a%3==0&& a%5==0)
+ {
+    console.log("fizzbuzz")
+ }
+ else if(a%3==0)
+ {
+    console.log("fizz")
+ }
+ else{
+    console.log("buzz")
+ }
